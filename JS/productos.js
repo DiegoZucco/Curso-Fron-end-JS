80a77a7c60def7cfc14ccafef00b5382df1fbc3f
Diego Zuccolilli <Dzuccolilli@gmail.com>
2025-07-15 22:55:20 -0300

    feat: Agregar página de productos con estructura HTML y enlaces a recursos

---
+++ JS/productos.js
@@ -0,0 +1,21 @@
+const productos=[
+
+  {
+    "ID": "AB1234",
+    "nombre_articulo": "",
+    "precio": "",
+    "foto": ""
+  },
+  {
+    "ID": "CD5678",
+    "nombre_articulo": "",
+    "precio": "",
+    "foto": ""
+  },
+  {
+    "ID": "EF9012",
+    "nombre_articulo": "",
+    "precio": "",
+    "foto": ""
+  }
+];
